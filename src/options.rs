bafb7816972c876555823cad5707f8e0808f9f7c
roife <roifewu@gmail.com>
2021-10-13 11:16:06 +0800

    [add] emit option

--- src/options.rs
+++ src/options.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use std::str::FromStr;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -12,9 +13,28 @@ pub struct Options {
     #[structopt(short, long, default_value = "a.out", parse(from_os_str))]
     pub output_file: PathBuf,
 
+    #[structopt(long, default_value = "ir")]
+    pub emit: EmitOption,
+
     #[structopt(short, long)]
     pub passes: Option<Vec<String>>,
 
     #[structopt(short, long, default_value = "warn")]
     pub log_level: log::LevelFilter,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum EmitOption {
+    Ir,
+}
+
+impl FromStr for EmitOption {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "ir" => Ok(EmitOption::Ir),
+            _ => Err("Allowed emit options: ir"),
+        }
+    }
 }
