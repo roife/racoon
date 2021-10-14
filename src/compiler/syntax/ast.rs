e8655c0a9e70c12d94b53abffa19d0fe7b11169f
roife <roifewu@gmail.com>
2021-10-14 11:12:18 +0800

    [new] ast for func

--- src/compiler/syntax/ast.rs
+++ src/compiler/syntax/ast.rs
@@ -1,47 +1,62 @@
 use std::rc::Rc;
 use super::{
-    span::{Span}
+    span::{Span},
 };
 
 #[derive(Debug, Clone)]
 pub struct Program {
     pub decls: Vec<DeclStmt>,
-    //pub funcs: Vec<FuncDef>,
+    pub funcs: Vec<FuncStmt>
 }
 
 #[derive(Debug, Clone)]
-pub enum DeclStmt {
-    Const(ConstDecl),
-    Var(VarDecl)
-}
-
-// impl DeclStmt {
-//     pub fn span(&self) -> Span {
-//         match self {
-//             DeclStmt::Const(x) => x.span,
-//             DeclStmt::Var(x) => x.span
-//         }
-//     }
-// }
-
-#[derive(Debug, Clone)]
-pub struct ConstDecl {
-    // pub name: Ident,
-    // pub ty: TypeDef,
-    // pub val: Rc<Expr>,
-    // pub span: Span,
+pub struct DeclStmt {
+    pub is_const: bool,
+    pub name: Ident,
+    pub ty: TyDef,
+    pub val: Option<Rc<Expr>>,
+    pub span: Span,
 }
 
 #[derive(Debug, Clone)]
-pub struct VarDecl {
-    // pub name: Ident,
-    // pub ty: TypeDef,
-    // pub val: Option<Rc<Expr>>,
-    // pub span: Span,
+pub struct FuncStmt {
+    pub name: Ident,
+    pub params: Vec<FuncParam>,
+    pub ret_ty: TyDef,
+    pub body: BlockStmt,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub struct FuncParam {
+    pub name: Ident,
+    pub ty: TyDef,
+}
+
+#[derive(Debug, Clone)]
+pub struct TyDef {
+    // todo
+    pub span: Span,
 }
 
 #[derive(Debug, Clone)]
 pub struct Ident {
-    pub span: Span,
     pub name: String,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub struct Expr {
+
+}
+
+#[derive(Debug, Clone)]
+pub struct BlockStmt {
+    pub stmts: Vec<Stmt>,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub struct Stmt {
+
 }
--- src/compiler/syntax/parser.rs
+++ src/compiler/syntax/parser.rs
@@ -1,11 +1,11 @@
 use std::iter::Peekable;
 
 use super::{
+    ast::*,
     err::ParseError,
-    span::Span,
-    token::{TokenType, Token},
     lexer::Lexer,
-    ast::*
+    span::Span,
+    token::{Token, TokenType},
 };
 
 macro_rules! expect_token {
@@ -18,20 +18,48 @@ macro_rules! expect_token {
     };
 }
 
-macro_rules! is_next {
+macro_rules! next_if_match {
     ($self:expr, $($pat:pat)|+) => {
-        $self.peek().map_or(false, |token| matches!(token, $($pat)|+))
+        $self.next_if(|token| matches!(token.token_type, $($pat)|+)).is_some()
     };
 }
 
+macro_rules! is_next {
+    ($self:expr, $($pat:pat)|+) => {
+        $self.peek().map_or(false, |token| matches!(token.token_type, $($pat)|+))
+    };
+}
+
+macro_rules! while_match_parse {
+    ($self:expr, $($sep_pat:pat)|+, $parse:expr) => {{
+        let mut v = vec![];
+        while next_if_match!($self, $($sep_pat)|+) {
+            let val = (|| $parse)()?;
+            v.push(val);
+        }
+        v
+    }};
+}
+
+macro_rules! until_match_parse {
+    ($self:expr, $($term_pat:pat)|+, $parse:expr) => {{
+        let mut v = vec![];
+        while !is_next!($self, $($term_pat)|+) {
+            let val = (|| $parse)()?;
+            v.push(val);
+        }
+        v
+    }};
+}
+
 pub struct Parser<T>
-    where T: Iterator<Item = char>,
+    where T: Iterator<Item=char>,
 {
     iter: Peekable<Lexer<T>>,
 }
 
 impl<T> Parser<T>
-    where T: Iterator<Item = char>,
+    where T: Iterator<Item=char>,
 {
     pub fn new(lexer: Lexer<T>) -> Parser<T> {
         Parser {
@@ -43,18 +71,91 @@ impl<T> Parser<T>
         self.parse_program()
     }
 
+    // CompUnit -> [CompUnit] (Decl | FuncDef)
     fn parse_program(&mut self) -> Result<Program, ParseError> {
+        let mut decls = vec![];
+        let mut funcs = vec![];
         loop {
+            let is_const = next_if_match!(self.iter, TokenType::ConstKw);
+            let ty = self.parse_ty()?;
+            let ident = self.parse_ident()?;
+            if is_next!(self.iter, TokenType::LParen) {
+                let func_stmt = self.parse_func_stmt(ty, ident)?;
+                funcs.push(func_stmt);
+            } else {
+                let decl_stmt = self.parse_decl_stmt(is_const, ty, ident)?;
+                decls.push(decl_stmt);
+            }
         }
-//        Ok(Program { decls, funcs })
+        Ok(Program {
+            decls,
+            funcs,
+        })
+    }
+
+    // FuncDef -> FuncType Ident '(' [FuncFParams] ')' Block
+    // FuncType -> 'void' | 'int'
+    // FuncFParams -> FuncFParam { ',' FuncFParam }
+    // todo: FuncFParam -> BType Ident ['[' ']' { '[' Exp ']' }]
+    fn parse_func_stmt(&mut self, ret_ty: TyDef, name: Ident) -> Result<FuncStmt, ParseError> {
+        let start = expect_token!(self.iter, TokenType::LParen)?.span.start;
+        let params = while_match_parse!(self.iter, TokenType::Comma, {
+            let ty = self.parse_ty()?;
+            let name = self.parse_ident()?;
+                Ok(FuncParam {
+                    name,
+                    ty
+                })
+            });
+        expect_token!(self.iter, TokenType::RParen)?;
+        let body = self.parse_block_stmt()?;
+        let end = body.span.end;
+
+        Ok(FuncStmt {
+            name,
+            params,
+            ret_ty,
+            body,
+            span: Span { start, end }
+        })
+    }
+
+    // Block -> '{' { BlockItem } '}'
+    fn parse_block_stmt(&mut self) -> Result<BlockStmt, ParseError> {
+        let start = expect_token!(self.iter, TokenType::LBrace)?.span.start;
+        let stmts = until_match_parse!(self.iter, TokenType::RBrace, self.parse_stmt());
+        let end = expect_token!(self.iter, TokenType::RBrace)?.span.end;
+        Ok(BlockStmt {
+            stmts,
+            span: Span { start, end },
+        })
+    }
+
+    fn parse_stmt(&mut self) -> Result<Stmt, ParseError> {
+        todo!()
+    }
+
+    // Decl -> ConstDecl | VarDecl
+    // ConstDecl -> 'const' BType ConstDef { ',' ConstDef } ';'
+    // VarDecl -> BType VarDef { ',' VarDef } ';'
+    fn parse_decl_stmt(&mut self, is_const: bool, ty: TyDef, first_ident: Ident) -> Result<DeclStmt, ParseError> {
+        todo!()
+    }
+
+    fn parse_expr(&mut self) -> Result<Expr, ParseError> {
+        todo!()
+    }
+
+    fn parse_ty(&mut self) -> Result<TyDef, ParseError> {
+        let token = expect_token!(self.iter, TokenType::VoidTy | TokenType::IntTy)?;
+        Ok(todo!())
     }
 
     fn parse_ident(&mut self) -> Result<Ident, ParseError> {
         let token = expect_token!(self.iter, TokenType::Ident(_))?;
         Ok(Ident {
             span: token.span,
-            name: token.token_type.own_ident_name().unwrap()
+            name: token.token_type.own_ident_name().unwrap(),
         })
     }
-
 }
