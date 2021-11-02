022d0e4a04317da005fada7af87a3ca90bf81a8b
roife <roifewu@gmail.com>
2021-11-02 21:34:45 +0800

    [add] lint

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,3 +1,13 @@
+#![forbid(unsafe_code)]
+#![warn(
+clippy::pedantic,
+missing_copy_implementations,
+missing_debug_implementations,
+rustdoc::broken_intra_doc_links,
+trivial_numeric_casts,
+unused_allocation
+)]
+
 mod span;
 mod intrusive_linkedlist;
 pub mod syntax;
